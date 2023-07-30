013014d5aa2e8e9bc4caf6e943a72475b3c2e1ed
liwei <277622557@qq.com>
2023-07-30 18:26:15 +0800

    鼠标移动批量选中

--- src/components/OperationPanel/index.jsx
+++ src/components/OperationPanel/index.jsx
@@ -2,15 +2,50 @@ import React, { Component } from 'react'
 import './index.css'
 
 export default class OperationPanel extends Component {
-   
+  flag = false //当鼠标被按下时，为true,放开是为true
+  indexs = [] //用来存放鼠标经过的单元格在整个表格的位置，鼠标按下时被初始化，
+    // 鼠标按下事件
+  onMouseDown =() =>{
+      this.flag = true;
+      this.indexs = [];
+  }
+  onMouseMove =  (e)=> {
+    // console.log(e)
+    if (this.flag)//只有鼠标被按下时，才会执行复合代码
+    {   
+      // console.log(this)
+    
+        this.indexs.push(this.search(e.target, document.getElementsByTagName("li")))
+    }
+ }
+ onMouseUp =() =>{
+    
+    // 去重
+    this.indexs = [...new Set(this.indexs)] 
+    //去掉undefined
+    this.indexs =  this.indexs.filter(item => item)
+    
+    this.indexs.map(ref =>{
+      console.log(ref)
+      this[ref].className = 'active'
+    })
+ }
+ search =(a, A)=> {
+  // console.log(a,A)
+    var length = A.length;
+    for (var i = 0; i < length; i++) {
+        if (a === A[i]) {
+            return a.id;
+        }
+    }
+  }
+  // 点击单元格切换选中状态
   timeClick = (range,i,j)=>{
     return (event)=>{
       range.active = !range.active
-      // console.log(i,j,event.target,range)
       this.props.updateData(range,i,j)
     }
   }
- 
   render() {
     const {week} = this.props
     // console.log(this.props)
@@ -18,14 +53,14 @@ export default class OperationPanel extends Component {
       <div>
         {week.map((day,i) => {
           return (
-            <div className='operation-content'   key={i}>
+            <div className='operation-content'   key={i} onMouseDown={this.onMouseDown} onMouseMove={this.onMouseMove} onMouseUp={this.onMouseUp}>
               <div className='week-day'>
-                <span className='font-css'>{day.dayName}</span>
+                <span className='font-css' style={{userSelect:'none'}}>{day.dayName}</span>
               </div>
               <ul className='content-right'>
-                {day.rangeList.map((range,j) =>{
+                {day.rangeList.map((range,j) =>{  
                   return(
-                      <li key={j} className={range.active?'active':''} onClick={this.timeClick(range,i,j)}></li>
+                      <li key={j} id={`cell${i}${j}`} ref={c => this[`cell${i}${j}`] = c } className={range.active?'active':''} onClick={this.timeClick(range,i,j)}></li>
                   )
                 })}
               </ul>
